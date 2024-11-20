71ca0f3228d26c3b48198ae060a025cf962b82b3
Tods <algy25ng@gmail.com>
2024-11-21 02:32:09 +0800

    active link function

---
+++ src/scripts/utility/active-link.js
@@ -0,0 +1,21 @@
+const setActiveLink = () => {
+  const currentPath = window.location.hash; // Mendapatkan hash dari URL (contoh: #/home)
+
+  // Ambil semua link dalam nav-menu
+  const navLinks = document.querySelectorAll('.nav-link');
+
+  navLinks.forEach((link) => {
+    // Periksa apakah href dari link cocok dengan currentPath
+    if (link.getAttribute('href') === currentPath) {
+      link.classList.add('active-link');
+    } else {
+      link.classList.remove('active-link');
+    }
+  });
+};
+
+// Jalankan fungsi saat halaman dimuat
+window.addEventListener('load', setActiveLink);
+
+// Jalankan fungsi saat hash berubah (untuk menangkap navigasi ke halaman lain)
+window.addEventListener('hashchange', setActiveLink);
--- src/scripts/utility/animation/home-page/footer-gsap.js
+++ src/scripts/utility/animation/home-page/footer-gsap.js
@@ -11,10 +11,10 @@ export default function footerGsapJs() {
     duration: 5.5,
     ease: 'power2.out',
     scrollTrigger: {
-      trigger: 'footer-bar', // Elemen container yang memicu animasi
+      trigger: 'footer', // Elemen container yang memicu animasi
       start: 'top 90%', // Mulai animasi saat container mencapai 80% dari viewport
       end: 'bottom 10%', // Selesai animasi saat container mencapai 20% dari viewport
-      toggleActions: 'play', // Mainkan animasi maju dan mundur
+      toggleActions: 'play none none reverse', // Mainkan animasi maju dan mundur
     },
   });
 }
