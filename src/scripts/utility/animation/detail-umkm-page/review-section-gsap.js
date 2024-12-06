55057317eed94c160fe0a8df57e156a3620f4ea1
Tods <algy25ng@gmail.com>
2024-12-06 16:06:48 +0800

    fixed bug in review card users own umkm in list umkm

--- src/scripts/utility/animation/detail-umkm-page/review-section-gsap.js
+++ src/scripts/utility/animation/detail-umkm-page/review-section-gsap.js
@@ -22,10 +22,9 @@ export default function detailUmkmReviewGsapJs() {
     x: 0,
     duration: 2,
     ease: 'power2.out',
-    stagger: 0.5,
     scrollTrigger: {
-      trigger: '.detailumkm-reviews', // Elemen container yang memicu animasi
-      start: 'top 70%', // Mulai animasi saat container mencapai 80% dari viewport
+      trigger: '.review-item', // Elemen container yang memicu animasi
+      start: 'top 90%', // Mulai animasi saat container mencapai 80% dari viewport
       end: 'bottom 20%', // Selesai animasi saat container mencapai 20% dari viewport
       toggleActions: 'play none none reverse', // Mainkan animasi maju dan mundur
     },
